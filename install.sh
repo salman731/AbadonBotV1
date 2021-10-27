5fbd29c9dc8f05bebcebe689bbebf15087598c2e
anxo2077 <78989681+anxo2077@users.noreply.github.com>
2021-10-27 01:25:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apt install figlet -y
 apt install wget -y
 apt install ffmpeg libwebp -y
 apt install nodejs -y
-apt install tesseract-ocr -y
+apt install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 npm audit fix
